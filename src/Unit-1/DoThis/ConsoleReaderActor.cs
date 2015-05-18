892bede5266b2e3a27f49e13bd0d307dc2eedab4
steve@stevedasilva.com <7qy4JITgEW7z>
2015-05-18 18:08:41 -0400

    Lesson 1.5

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -11,12 +11,6 @@ namespace WinTail
     {
         public const string StartCommand = "start";
         public const string ExitCommand = "exit";
-        private IActorRef _validationActor;
-
-        public ConsoleReaderActor(IActorRef validationActor)
-        {
-            _validationActor = validationActor;
-        }
 
         protected override void OnReceive(object message)
         {
@@ -51,7 +45,7 @@ namespace WinTail
             }
 
             // otherwise, just had message off to validation actor (by telling its actor ref)
-            _validationActor.Tell(message);
+            Context.ActorSelection("akka://MyActorSystem/user/validationActor").Tell(message);
         }
         #endregion
     }
